4542e973e8978a44df65787bf75bb70ee543323f
zhangbin <zhangbin@mizhua.com>
2019-08-22 11:09:20 +0800

    删除pod

--- Example/Pods/Target Support Files/A0PreMainTime/A0PreMainTime-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_A0PreMainTime : NSObject
-@end
-@implementation PodsDummy_A0PreMainTime
-@end
--- Example/Pods/Target Support Files/Pods-A0PreMainTime_Example/Pods-A0PreMainTime_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_A0PreMainTime_Example : NSObject
-@end
-@implementation PodsDummy_Pods_A0PreMainTime_Example
-@end
